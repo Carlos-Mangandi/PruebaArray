4ea86e27e4a956653e9618cc1fed4541298f58af
Carlos Mangandí <me22001@esfe.agape.edu.sv>
2023-10-14 12:41:42 -0600

    Realización de prueba de métodos array

---
+++ prueba.js
@@ -0,0 +1,91 @@
+// 1 finalizado
+// const arreglo = [10, "5", "7", 9, "23", "17"];
+// const numeros = arreglo.filter(elemento => typeof elemento === 'number' || !isNaN(elemento)).map(Number);
+// const resultado = numeros.reduce((acumulador, numero) => acumulador * numero, 1);
+
+// console.log(resultado);
+ 
+//2 finalizado
+// const array = ["manzana","pera","mandarina","limon"]
+// const result = array.filter((x) => x.length > 6)
+
+// console.log(result)
+
+// 3 finalizado
+// const numero = 7;
+// const arreglo = [];
+
+// for (let i = 1; i <= 10; i++) {
+//   arreglo.push(numero * i);
+// }
+
+// arreglo.forEach(function (resultado, indice) {
+//   console.log(`${numero} x ${indice + 1} = ${resultado}`);
+// });
+
+// 4 finalizado
+// const arreglo = ["manzana", "limon","sandia", "pera","toronja","naranja"]
+// const result = arreglo.includes("sandia")
+
+// console.log(result)
+// console.log(arreglo)
+
+// 5-a finalizado
+// const Usuarios = [
+//         {
+//             name: "Rafael Perez",
+//             email: "rafael@gmail.com"
+//         },
+//         {
+//             name: "Rosario Perez",
+//             email: "rosario@gmail.com"
+//         },
+//         {
+//             name: "Rodrigo Avila",
+//             email: "rodrigo@gmail.com"
+//         },
+//         {
+//             name: "Alberto Campos",
+//             email: "albertogo@gmail.com"
+//         }
+//     ];
+    
+//     const result = Usuarios.some(Usuarios=>Usuarios.email === 'rosario@gmail.com');
+//     console.log(result);
+
+// 5-b finalizado
+// const arreglo = [1,6,3,4,2,56,10]
+// const result = arreglo.sort((a,b) => b-a)
+// console.log(result)
+
+// 6 finalizado
+// const numero = [1, 2, 3, 4, 5, 6, 7]
+// const result = numero.every(numero => numero > 5)
+
+// console.log(result)
+
+// 7 finalizado
+// const arreglo1 = [1,2,3]
+// const arreglo2 = ["pera","melon","naranja"]
+// const arreglo3 = [true, false]
+// const result = arreglo1.concat(arreglo2, arreglo3)
+// console.log(result)
+
+// 8 finalizado
+// const arreglo = [1,2,3,4,5]
+// const result = arreglo.shift()
+
+// console.log(result)
+// console.log(arreglo)
+
+// 9  finalizado
+// const arreglo = [12, 6, 7, 8, 11, 10, 9, 5, 3, 43];
+// const result = arreglo.indexOf(5);
+
+// console.log(`${result}`);
+
+// 10 finalizado
+// const arreglo = [1,2,3,4,5,6,7,8,9,10]
+// const result = arreglo.map((x) => x * 3)
+
+// console.log(result);
